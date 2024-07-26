1291989e4bd9f9e72a4742a057627ca9980d7de1
SnowCait <SnowCait@users.noreply.github.com>
2024-07-26 12:21:19 +0900

    Error: Verification request was timed out.

--- rx-nostr/src/routes/web-worker/+page.svelte
+++ rx-nostr/src/routes/web-worker/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import type { Event } from 'nostr-typedef';
-	import { createRxBackwardReq, createRxNostr, uniq } from 'rx-nostr';
+	import { createRxBackwardReq, createRxNostr, filterByKind, latestEach, uniq } from 'rx-nostr';
 	import { createNoopClient, createVerificationServiceClient } from 'rx-nostr-crypto';
 	import { onDestroy, onMount } from 'svelte';
 	import workerUrl from '$lib/worker?worker&url';
 	import { relays } from '$lib/Constants';
+	import { share, tap } from 'rxjs';
 
 	const events = new Map<string, Event>();
 
@@ -22,13 +23,19 @@
 	const req = createRxBackwardReq();
 	const subscription = rxNostr
 		.use(req)
-		.pipe(uniq())
+		.pipe(
+			uniq(),
+			tap(({ event }) => references(event, 1))
+		)
 		.subscribe({
-			next: ({ event, from }) => {
-				console.log(from, event);
+			next: ({ event }) => {
+				console.log('[kind 1]', 0, event.id);
 				events.set(event.id, event);
 			},
-			complete: () => console.log('[count]', events.size)
+			complete: () => console.log('[count]', events.size),
+			error: (error) => {
+				console.error('[kind 1]', 0, error);
+			}
 		});
 
 	onMount(() => {
@@ -41,6 +48,55 @@
 		console.log('[on destroy]');
 		subscription.unsubscribe();
 	});
+
+	function references(event: Event, depth = 0): void {
+		const ids = event.tags.filter(([t]) => t === 'e').map(([, id]) => id);
+
+		const req = createRxBackwardReq();
+		const observable$ = rxNostr.use(req).pipe(uniq(), share());
+		observable$
+			.pipe(
+				filterByKind(0),
+				latestEach(({ event }) => event.pubkey)
+			)
+			.subscribe({
+				next: ({ event }) => {
+					console.log('[kind 0]', depth, event.id);
+				},
+				complete: () => {
+					console.log('[kind 0]', depth, 'complete');
+				},
+				error: (error) => {
+					console.error('[kind 0]', depth, error);
+				}
+			});
+		observable$
+			.pipe(
+				filterByKind(1),
+				tap(({ event }) => references(event, depth + 1))
+			)
+			.subscribe({
+				next: ({ event }) => {
+					console.log('[kind 1]', depth, event.id);
+				},
+				complete: () => {
+					console.log('[kind 1]', depth, 'complete');
+				},
+				error: (error) => {
+					console.error('[kind 1]', depth, error);
+				}
+			});
+		req.emit([
+			{
+				kinds: [0],
+				authors: [event.pubkey]
+			},
+			{
+				ids
+			}
+		]);
+		req.over();
+	}
 </script>
 
 <h1>Web Worker</h1>
