3afe65f4051d346e55c11e92bcd83583c7445ee4
SnowCait <SnowCait@users.noreply.github.com>
2024-09-15 00:09:08 +0900

    Redirect to profile page

--- svelte5/src/routes/html5-qrcode/+page.svelte
+++ svelte5/src/routes/html5-qrcode/+page.svelte
@@ -1,26 +1,9 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { Html5QrcodeScanner } from 'html5-qrcode';
-	import { nip19 } from 'nostr-tools';
 	import { onMount } from 'svelte';
 
-	let nprofile = '';
-	let pubkey = '';
-
-	$: if (nprofile) {
-		const { type, data } = nip19.decode(nprofile.substring('nostr:'.length));
-		switch (type) {
-			case 'npub': {
-				pubkey = data;
-				break;
-			}
-			case 'nprofile': {
-				pubkey = data.pubkey;
-				break;
-			}
-		}
-	}
-
-	onMount(() => {
+	$: onMount(() => {
 		const scanner = new Html5QrcodeScanner(
 			'reader',
 			{
@@ -31,14 +14,12 @@
 		);
 		scanner.render((text: string) => {
 			console.log('[qr]', text);
-			if (text.startsWith('nostr:')) {
-				nprofile = text;
+			if (text.startsWith('nostr:npub')) {
 				scanner.pause(true);
+				goto(`https://nostter.app/${text.substring('nostr:'.length)}`);
 			}
 		}, undefined);
 	});
 </script>
 
 <div id="reader"></div>
-
-<div>{pubkey}</div>
