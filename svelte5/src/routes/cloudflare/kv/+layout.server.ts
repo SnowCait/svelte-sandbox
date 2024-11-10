b5dd94543dbf6a49e10223e567e8e1059534aa40
SnowCait <SnowCait@users.noreply.github.com>
2024-11-10 15:56:38 +0900

    KV

--- svelte5/src/routes/cloudflare/kv/+layout.server.ts
+++ svelte5/src/routes/cloudflare/kv/+layout.server.ts
@@ -1,18 +1,26 @@
 import type { LayoutServerLoad } from './$types';
-import { CLOUDFLARE_API_TOKEN, CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_KV_NAMESPACE_ID } from '$env/static/private';
-import { error } from '@sveltejs/kit';
+import {
+	CLOUDFLARE_API_TOKEN,
+	CLOUDFLARE_ACCOUNT_ID,
+	CLOUDFLARE_KV_NAMESPACE_ID
+} from '$env/static/private';
 
 export const load: LayoutServerLoad = async () => {
+	if (CLOUDFLARE_API_TOKEN === undefined || CLOUDFLARE_ACCOUNT_ID === undefined || CLOUDFLARE_KV_NAMESPACE_ID === undefined) {
+		return {};
+	}
 	const key = 'test';
 	const url = `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/storage/kv/namespaces/${CLOUDFLARE_KV_NAMESPACE_ID}/values/${key}`;
 	const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${CLOUDFLARE_API_TOKEN}`
-        }
-    });
+		headers: {
+			Authorization: `Bearer ${CLOUDFLARE_API_TOKEN}`
+		}
+	});
 	console.log(response.status, response.statusText);
-	if (response.ok) {
-		console.log(await response.text());
-        error(403);
-	}
+	return {
+		ok: response.ok,
+		status: response.status,
+		statusText: response.statusText,
+		body: await response.text()
+	};
 };
--- svelte5/src/routes/cloudflare/kv/+page.svelte
+++ svelte5/src/routes/cloudflare/kv/+page.svelte
@@ -1,1 +1,9 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+</script>
+
 <h1>Cloudflare KV</h1>
+
+<pre>{JSON.stringify(data, null, 2)}</pre>
