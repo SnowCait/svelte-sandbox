5b2b0d926b8bbbf72326beb34b0ecedd0c8170f7
SnowCait <SnowCait@users.noreply.github.com>
2024-11-18 21:38:04 +0900

    Runes

--- svelte5/src/routes/runes/+page.svelte
+++ svelte5/src/routes/runes/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-	import { SvelteMap } from 'svelte/reactivity';
+	import { SvelteMap, SvelteURL } from 'svelte/reactivity';
 
 	let count = $state(0);
 	let map = new SvelteMap<number, number>();
+	let url = new SvelteURL('wss://example.com/');
 	setInterval(() => {
 		count++;
 		map.set(count, Math.random());
 	}, 500);
+	setTimeout(() => {
+		url.protocol = 'https:';
+	}, 5000);
 </script>
 
 <div>{count}</div>
-<dvi>{map.get(10) ?? '?'}</dvi>
+<div>{map.get(10) ?? '?'}</div>
+<div>{url.href}</div>
