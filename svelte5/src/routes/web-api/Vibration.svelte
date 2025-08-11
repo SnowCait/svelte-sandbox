e186426f2ac801ff5888ca7dd91dc0eeb474c5de
SnowCait <SnowCait@users.noreply.github.com>
2025-08-12 04:12:22 +0900

    Vibration

--- svelte5/src/routes/web-api/Vibration.svelte
+++ svelte5/src/routes/web-api/Vibration.svelte
@@ -1,7 +1,5 @@
-<script lang="ts">
-	function vibrate() {
-		navigator.vibrate(200);
-	}
-</script>
-
-<button onclick={vibrate}>Vibrate</button>
+{#if navigator.vibrate}
+	<button onclick={() => navigator.vibrate(200)}>Vibrate</button>
+{:else}
+	<p>Vibration API is not supported on this device.</p>
+{/if}
