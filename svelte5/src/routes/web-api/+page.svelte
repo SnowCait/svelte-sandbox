8a9645d4af40338a6a3023adf8e28b18cdf8be06
SnowCait <SnowCait@users.noreply.github.com>
2025-10-31 05:04:39 +0900

    Fetch API

--- svelte5/src/routes/web-api/+page.svelte
+++ svelte5/src/routes/web-api/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import Fetch from './Fetch.svelte';
 	import Navigator from './Navigator.svelte';
 	import Vibration from './Vibration.svelte';
 </script>
 
 <h1>Web API</h1>
+
+<h2>Fetch</h2>
+<Fetch />
 
 <h2>Vibration</h2>
 <Vibration />
---
+++ svelte5/src/routes/web-api/Fetch.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let url = $state('');
+	let text = $state('');
+
+	async function request(e: SubmitEvent) {
+		e.preventDefault();
+		const response = await fetch(url);
+		text = await response.text();
+	}
+</script>
+
+<form onsubmit={request}>
+	<input type="url" bind:value={url} />
+	<input type="submit" />
+</form>
+
+<pre>{text}</pre>
