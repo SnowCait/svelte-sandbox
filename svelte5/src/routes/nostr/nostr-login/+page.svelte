548d2b375b65607bd8670445c9df6bcfc85ace38
SnowCait <SnowCait@users.noreply.github.com>
2025-12-01 21:24:49 +0900

    Nostr-Login

--- svelte5/src/routes/nostr/nostr-login/+page.svelte
+++ svelte5/src/routes/nostr/nostr-login/+page.svelte
@@ -2,14 +2,13 @@
 	import { onMount } from 'svelte';
 
 	onMount(async () => {
-		document.addEventListener('nlAuth', ((event: CustomEvent) => {
-			console.log('[nostr-login]', event.detail);
-		}) as EventListener);
+		const { init } = await import('nostr-login');
+		await init({
+			onAuth: (npub) => {
+				console.log('[on auth]', npub);
+			}
+		});
 	});
 </script>
-
-<svelte:head>
-	<script src="https://www.unpkg.com/nostr-login@latest/dist/unpkg.js"></script>
-</svelte:head>
 
 <h1>Nostr-Login</h1>
