566db45fa0d954b40e91359e2328ce91e939783a
SnowCait <SnowCait@users.noreply.github.com>
2025-12-12 11:01:10 +0900

    Window Events

--- svelte5/src/routes/web-api/+page.svelte
+++ svelte5/src/routes/web-api/+page.svelte
@@ -2,9 +2,13 @@
 	import Fetch from './Fetch.svelte';
 	import Navigator from './Navigator.svelte';
 	import Vibration from './Vibration.svelte';
+	import WindowEvents from './WindowEvents.svelte';
 </script>
 
 <h1>Web API</h1>
+
+<h2>Window Events</h2>
+<WindowEvents />
 
 <h2>Fetch</h2>
 <Fetch />
---
+++ svelte5/src/routes/web-api/WindowEvents.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	const logs = $state<string[]>([]);
+
+	function logEvent(event: Event): void {
+		if ('Notification' in window) {
+			new Notification(event.type);
+		}
+		logs.push(`${event.type}, ${document.visibilityState}`);
+	}
+</script>
+
+<svelte:window on:visibilitychange={logEvent} on:pagehide={logEvent} on:pageshow={logEvent} />
+
+<ul>
+	{#each logs as log}
+		<li>{log}</li>
+	{/each}
+</ul>
