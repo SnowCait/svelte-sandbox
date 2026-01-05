86f7a3722911e0c64cf23add80945cb2fb5a8402
SnowCait <SnowCait@users.noreply.github.com>
2026-01-06 08:14:20 +0900

    Virtual List

---
+++ svelte5/src/lib/VirtualList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" generics="T">
+    let items: T[] = [];
+    let itemsForView: T[] = [];
+    export function add(newItems: T[], index: number = Infinity): void {
+        if (index>items.length) {
+        items.push(...newItems);
+        } else {
+            items.splice(index, 0, ...newItems);
+        }
+    }
+</script>
+
+{#each items as item}
+    <slot>
+        <div>{item}</div>
+    </slot>
+{/each}
