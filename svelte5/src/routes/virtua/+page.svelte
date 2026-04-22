a77a8a62a82b42653b79d57d7039a0b7acd1f591
SnowCait <SnowCait@users.noreply.github.com>
2026-04-22 14:17:01 +0900

    Virtua WindowVirtualizer

--- svelte5/src/routes/virtua/+page.svelte
+++ svelte5/src/routes/virtua/+page.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
-	import { VList } from 'virtua/svelte';
+	import { VList, WindowVirtualizer } from 'virtua/svelte';
 
 	const sizes = [20, 40, 180, 77];
-	const data = Array.from({ length: 1000 }).map((_, i) => sizes[i % 4]);
+	const data = Array.from({ length: 100000 }).map((_, i) => sizes[i % 4]);
 </script>
 
 <h1>Virtua</h1>
 
-<VList {data} style="height: 100vh" getKey={(_, i) => i}>
+<!-- リストの領域だけスクロール -->
+<!-- <VList {data} style="height: 100vh" getKey={(_, i) => i}>
 	{#snippet children(item, index)}
 		<div style="height: {item}px;" class="item">{index}</div>
 	{/snippet}
-</VList>
+</VList> -->
+
+<!-- ウィンドウ全体をスクロール -->
+<WindowVirtualizer {data} getKey={(_, i) => i}>
+	{#snippet children(item, index)}
+		<div style="height: {item}px;" class="item">{index}</div>
+	{/snippet}
+</WindowVirtualizer>
 
 <style>
 	.item {
